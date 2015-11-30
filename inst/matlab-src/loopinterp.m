912dbb2c56c061d82b0dc8ac218510a9a104043a
mkoohafkan <michael.koohafkan@gmail.com>
2015-11-30 07:20:28 -0800

    updated vignettes

--- inst/matlab-src/loopinterp.m
+++ inst/matlab-src/loopinterp.m
@@ -7,5 +7,5 @@ for i = 1:length(fname)
   load([fpath '/' fname{i}]) ;
   ctd_interp = interpctd(ctd, 'dist', 'elev', {'sa' 'ta' 'oa'}, ...
     0.1, 0.1, 'natural', 'nearest') ;
-  save([fpath '_interp/' fname{i}(1:end-4) '_interp.mat'], 'ctd_interp') ;
+  %save([fpath '_interp/' fname{i}(1:end-4) '_interp.mat'], 'ctd_interp') ;
 end
